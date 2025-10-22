f497079af6f4c66bf19d72c6a1f6190b0f716a1f
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-10-22 15:29:47 +0530

    used type conversion on calc

--- type-conversion/type-conversion.go
+++ type-conversion/type-conversion.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func calc() {
+	var a int = 10
+	var b float64 = 5.5
+
+	var sum float64 = float64(a) + b
+	fmt.Println("Sum as float:", sum)
+
+	var sum2 int = a + int(b)
+	fmt.Println("sum as int = ", sum2)
+
+}
+
 func main() {
 	var intVar int = 42
 	var floatVar float64 = 3.14
@@ -12,7 +24,10 @@ func main() {
 	// Type conversion...................................
 	var newFloatVar float64 = float64(intVar)
 	var newIntVar int = int(floatVar)
+	// ................................................
 
 	fmt.Println("Converted Integer:", newIntVar)
 	fmt.Println("Converted Float:", newFloatVar)
+
+	calc()
 }
